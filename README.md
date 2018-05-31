cf9c2263e3ff5fd00519d67b7aa3290277230175
Deepak Pathak <dkp24110@gmail.com>
2018-05-31 09:04:55 +0530

    first commit

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# Linux_CLI_YTD
-# Linux_CLI_YTD
-adding files
-adding files
-adding files
-adding files
+This is my personal project. In this project, I make command line application to download videos using `youtube-dl` command. 
+
+TODO - 
+Make UI for this. 
--- two.sh
+++ two.sh
@@ -1,6 +1,6 @@
 # git remote set-url origin git@github.com:dkp-1024/Linux_CLI_YTD.git
-echo "adding files" >> README.md
-git init
+# echo "adding files" >> README.md
+# git init
 git add . #add everything in this folder
 git commit -m "first commit"
 git remote add origin https://github.com/dkp-1024/Linux_CLI_YTD.git
