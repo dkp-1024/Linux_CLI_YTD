721f50aaedf3bf347657539f224dc170d072e9e3
Deepak Pathak <dkp24110@gmail.com>
2018-05-31 10:02:02 +0530

    first commit

--- two.sh
+++ two.sh
@@ -3,5 +3,11 @@
 # git init
 git add . #add everything in this folder
 git commit -m "first commit"
-git remote add origin https://github.com/dkp-1024/Linux_CLI_YTD.git
+# git remote add origin https://github.com/dkp-1024/Linux_CLI_YTD.git
+git add origin https://github.com/dkp-1024/Linux_CLI_YTD.git
 git push -u origin master
+# git config --global credential.helper cache
+# Set git to use the credential memory cache
+
+# git config --global credential.helper 'cache --timeout=3600'
+# Set the cache to timeout after 1 hour (setting is in seconds)
